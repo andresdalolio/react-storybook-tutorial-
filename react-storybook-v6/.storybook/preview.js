5973dc7df61cbe68cea71196ceca4221a1b1a067
Andres Dalolio <amda95@hotmail.com>
2021-08-12 09:27:44 -0600

    Decorators storybook (local and global)

--- react-storybook-v6/.storybook/preview.js
+++ react-storybook-v6/.storybook/preview.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import Center from '../src/components/Center/Center';
+
+addDecorator(story => <Center>{story()}</Center>); //global way to set decorator
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- react-storybook-v6/src/components/Button/Button.stories.js
+++ react-storybook-v6/src/components/Button/Button.stories.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Small } from '../Input/Input.stories';
 import Button from './Button';
+import Center from '../Center/Center';
 
 export default {
     title: 'Form/control/Button',
     component: Button,
     args: {
         children: 'Button' //set args at a components level. Args at story levek overrides args at component level.
-    }
+    },
+    decorators: [story => <Center>{story()}</Center>] //this adds decorator locally
 }
 
 export const Primary = () => (
-    <Button variant='primary'>
-        Primary
-    </Button>
+    <Center>
+        <Button variant='primary'>
+            Primary
+        </Button>
+    </Center>
 );
 
 export const Secondary = () => (
